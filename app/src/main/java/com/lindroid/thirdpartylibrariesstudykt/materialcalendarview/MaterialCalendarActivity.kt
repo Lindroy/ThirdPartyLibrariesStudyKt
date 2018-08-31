7902ab7917bb36023893cd5ce80a8e6c63af9bac
Lindroy <linyulong2014@163.com>
2018-08-31 11:16:12 +0800

    实现了给MaterialCalendar的周末文字设置不同颜色的功能。

--- app/src/main/java/com/lindroid/thirdpartylibrariesstudykt/materialcalendarview/MaterialCalendarActivity.kt
+++ app/src/main/java/com/lindroid/thirdpartylibrariesstudykt/materialcalendarview/MaterialCalendarActivity.kt
@@ -3,6 +3,7 @@ package com.lindroid.thirdpartylibrariesstudykt.materialcalendarview
 import android.util.Log
 import com.lindroid.thirdpartylibrariesstudykt.R
 import com.lindroid.thirdpartylibrariesstudykt.base.BaseActivity
+import com.lindroid.thirdpartylibrariesstudykt.materialcalendarview.decorator.WeekendDecorator
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.MaterialCalendarView
 import com.prolificinteractive.materialcalendarview.format.TitleFormatter
@@ -18,11 +19,14 @@ class MaterialCalendarActivity : BaseActivity() {
         super.initView()
         initToolBar(getString(R.string.material_calendarview))
         calendarView.setOnDateChangedListener { materialCalendarView, calendarDay, b ->
-            Log.d(TAG, "calendarDay=$calendarDay")
+            Log.e(TAG,"选择的日期："+"${calendarDay.year}-${calendarDay.month}-${calendarDay.day}")
+//            Log.d(TAG, "calendarDay=$calendarDay")
         }
         //设置选择模式：单选、多选和范围选择
         calendarView.selectionMode = MaterialCalendarView.SELECTION_MODE_SINGLE
         calendarView.setTitleFormatter(TitleFormat())
+
+        calendarView.addDecorator(WeekendDecorator())
     }
 
     class TitleFormat : TitleFormatter {
---
+++ app/src/main/java/com/lindroid/thirdpartylibrariesstudykt/materialcalendarview/decorator/WeekendDecorator.kt
@@ -0,0 +1,30 @@
+package com.lindroid.thirdpartylibrariesstudykt.materialcalendarview.decorator
+
+import android.graphics.Color
+import android.text.style.ForegroundColorSpan
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import com.prolificinteractive.materialcalendarview.DayViewDecorator
+import com.prolificinteractive.materialcalendarview.DayViewFacade
+import java.util.*
+
+/**
+ * @author Lin
+ * @date 2018/8/31
+ * @function
+ * @Description
+ */
+class WeekendDecorator:DayViewDecorator{
+    private val calendar = Calendar.getInstance(Locale.getDefault())
+    private val color = Color.parseColor("#ff0000")
+
+    override fun shouldDecorate(day: CalendarDay?): Boolean {
+        day?.copyTo(calendar)
+        val weekDay= calendar.get(Calendar.DAY_OF_WEEK)
+        return weekDay == Calendar.SATURDAY || weekDay == Calendar.SUNDAY
+    }
+
+    override fun decorate(view: DayViewFacade) {
+        view.addSpan(ForegroundColorSpan(color))
+    }
+
+}
